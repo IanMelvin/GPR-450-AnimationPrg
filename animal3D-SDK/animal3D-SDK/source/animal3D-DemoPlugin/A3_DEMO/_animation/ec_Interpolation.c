ae1e11fa1cf93c35a83d5b859864f85824a878c4
Robert Christensen <robert.christensen@mymail.champlain.edu>
2023-03-03 09:23:33 -0500

    Better comments

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.c
@@ -20,24 +20,24 @@ void vtable_setDefaults(ec_DataVtable* out)
 void* defaultLerp(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs)
 {
 	funcs->copy(val_out, v0);            //val_out = v0
-	funcs->invert(val_out);              //val_out = Invert(v0)
-	funcs->concat(val_out, v1, val_out); //val_out = v1 Concat Invert(v0)
-	funcs->scale(val_out, param);        //val_out = (v1 Concat Invert(v0)) Scale u
-	funcs->concat(val_out, val_out, v0); //val_out = ((v1 Concat Invert(v0)) Scale u) Concat v0
-	return val_out;
-}
-
-void* defaultDeconcat(void* val_out, const void* lhs, const void* rhs, const ec_DataVtable* funcs)
-{
-	funcs->copy(val_out, rhs);
-	funcs->invert(val_out);
-	funcs->concat(val_out, lhs, val_out);
+	funcs->invert(val_out);              //val_out = v0^-1
+	funcs->concat(val_out, v1, val_out); //val_out = v1 * v0^-1
+	funcs->scale(val_out, param);        //val_out = (v1 * v0^-1) * u
+	funcs->concat(val_out, val_out, v0); //val_out = ((v1 * v0^-1) * u) * v0
 	return val_out;
 }
 
 void* defaultNearest(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs)
 {
-	return funcs->lerp(val_out, v0, v1, param>0.5f ? 1.0f : 0.0f, funcs);
+	return funcs->lerp(val_out, v0, v1, param > 0.5f ? 1.0f : 0.0f, funcs);
+}
+
+void* defaultDeconcat(void* val_out, const void* lhs, const void* rhs, const ec_DataVtable* funcs)
+{
+	funcs->copy(val_out, rhs);            //val_out = rhs
+	funcs->invert(val_out);               //val_out = rhs^-1
+	funcs->concat(val_out, lhs, val_out); //val_out = lhs * rhs^-1
+	return val_out;
 }
 
 #pragma endregion
