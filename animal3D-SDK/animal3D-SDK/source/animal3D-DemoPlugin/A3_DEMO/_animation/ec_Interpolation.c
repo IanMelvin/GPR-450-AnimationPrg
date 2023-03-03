2a880cc2fc2086c5c9ccf139f731d30e4a400da2
Robert Christensen <robert.christensen@mymail.champlain.edu>
2023-03-03 10:07:40 -0500

    Started blocking out vtables using existing functions from A3DM. In setupVtables, temporarily commented out functions with missing definitions.

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.c
@@ -7,6 +7,7 @@
 
 void vtable_setDefaults(ec_DataVtable* out)
 {
+	out->copy       = defaultCopy      ;
 	out->lerp       = defaultLerp      ;
 	out->nearest    = defaultNearest   ;
 	out->cubic      = defaultCubic     ;
@@ -17,9 +18,15 @@ void vtable_setDefaults(ec_DataVtable* out)
 	out->biCubic    = defaultBiCubic   ;
 }
 
+void* defaultCopy(void* dst, const void* src, const ec_DataVtable* funcs)
+{
+	memcpy(dst, src, funcs->size);
+	return dst;
+}
+
 void* defaultLerp(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs)
 {
-	funcs->copy(val_out, v0);            //val_out = v0
+	funcs->copy(val_out, v0, funcs);     //val_out = v0
 	funcs->invert(val_out);              //val_out = v0^-1
 	funcs->concat(val_out, v1, val_out); //val_out = v1 * v0^-1
 	funcs->scale(val_out, param);        //val_out = (v1 * v0^-1) * u
@@ -29,12 +36,12 @@ void* defaultLerp(void* val_out, const void* v0, const void* v1, const a3real pa
 
 void* defaultNearest(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs)
 {
-	return funcs->copy(val_out, param>0.5f ? v1 : v0);
+	return funcs->copy(val_out, param>0.5f ? v1 : v0, funcs);
 }
 
 void* defaultDeconcat(void* val_out, const void* lhs, const void* rhs, const ec_DataVtable* funcs)
 {
-	funcs->copy(val_out, rhs);            //val_out = rhs
+	funcs->copy(val_out, rhs, funcs);     //val_out = rhs
 	funcs->invert(val_out);               //val_out = rhs^-1
 	funcs->concat(val_out, lhs, val_out); //val_out = lhs * rhs^-1
 	return val_out;
@@ -60,7 +67,79 @@ void* defaultBiNearest(void* val_out, const void* v00, const void* v01, const vo
 
 #pragma endregion
 
+#pragma region Vtable implementations
+
+#include <animal3D-A3DM/animal3D-A3DM.h>
+#include "a3_SpatialPose.h"
+
+ec_DataVtable vtable_mat4;
+ec_DataVtable vtable_SpatialPose;
+ec_DataVtable vtable_translation; // vec3, additive
+ec_DataVtable vtable_quatRotation; // quat, multiplicative
+ec_DataVtable vtable_eulerRotation; // vec3, additive
+ec_DataVtable vtable_scale; // vec3, multiplicative
+
 void setupVtables()
 {
+#ifdef _DEBUG
+	//Safety: Zero all to detect unset values
+	memset(&vtable_mat4         , 0, sizeof(ec_DataVtable));
+	memset(&vtable_SpatialPose  , 0, sizeof(ec_DataVtable));
+	memset(&vtable_translation  , 0, sizeof(ec_DataVtable));
+	memset(&vtable_quatRotation , 0, sizeof(ec_DataVtable));
+	memset(&vtable_eulerRotation, 0, sizeof(ec_DataVtable));
+	memset(&vtable_scale        , 0, sizeof(ec_DataVtable));
+#endif
+
+	//Set all defaults, size
+	vtable_setDefaults(&vtable_mat4         );
+	vtable_setDefaults(&vtable_SpatialPose  );
+	vtable_setDefaults(&vtable_translation  );
+	vtable_setDefaults(&vtable_quatRotation );
+	vtable_setDefaults(&vtable_eulerRotation);
+	vtable_setDefaults(&vtable_scale        );
+	vtable_mat4         .size = sizeof(a3mat4);
+	vtable_SpatialPose  .size = sizeof(a3_SpatialPose);
+	vtable_translation  .size = sizeof(a3vec3);
+	vtable_quatRotation .size = sizeof(a3quat);
+	vtable_eulerRotation.size = sizeof(a3vec3);
+	vtable_scale        .size = sizeof(a3vec3);
+
+	//mat4
+	vtable_mat4.identity = (fp_identity) a3real4x4SetIdentity;
+	vtable_mat4.invert	 = (fp_invert  ) a3real4x4Invert;
+	vtable_mat4.concat	 = (fp_concat  ) a3real4x4Product;
+	vtable_mat4.scale	 = (fp_scale   ) a3real4x4MulS;
+
+	//spatial pose
+	vtable_SpatialPose.identity = (fp_identity) a3spatialPoseReset;
+	//vtable_SpatialPose.invert   = (fp_invert  ) ;
+	vtable_SpatialPose.concat   = (fp_concat  ) a3spatialPoseConcat;
+	//vtable_SpatialPose.scale    = (fp_scale   ) ;
 	
+	//translation
+	//vtable_translation.identity = (fp_identity) ;
+	vtable_translation.invert	= (fp_invert  ) a3real3Negate;
+	vtable_translation.concat	= (fp_concat  ) a3real3Sum;
+	vtable_translation.scale	= (fp_scale   ) a3real3MulS;
+
+	//quat rotation
+	vtable_quatRotation.identity = (fp_identity) a3quatSetIdentity;
+	vtable_quatRotation.invert	 = (fp_invert  ) a3quatInvert;
+	//vtable_quatRotation.concat	 = (fp_concat  ) ;
+	//vtable_quatRotation.scale	 = (fp_scale   ) ;
+
+	//euler rotation
+	//vtable_eulerRotation.identity = (fp_identity) ;
+	vtable_eulerRotation.invert	  = (fp_invert  ) a3real3Negate;
+	vtable_eulerRotation.concat	  = (fp_concat  ) a3real3Sum;
+	vtable_eulerRotation.scale	  = (fp_scale   ) a3real3MulS;
+
+	//scale
+	//vtable_scale.identity = (fp_identity) ;
+	//vtable_scale.invert	  = (fp_invert  ) ;
+	vtable_scale.concat	  = (fp_concat  ) a3real3MulComp;
+	//vtable_scale.scale	  = (fp_scale   ) ;
 }
+
+#pragma endregion
--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/ec_Interpolation.h
@@ -14,7 +14,7 @@
 
 typedef struct ec_DataVtable ec_DataVtable;
 
-void vtable_setDefaults(ec_DataVtable* out); // Sets defaults for order 2+
+void vtable_setDefaults(ec_DataVtable* out); // Sets defaults for order 2+ (any that take a vtable ptr)
 void setupVtables();
 extern ec_DataVtable vtable_mat4;
 extern ec_DataVtable vtable_SpatialPose;
@@ -23,19 +23,24 @@ extern ec_DataVtable vtable_quatRotation; // quat, multiplicative
 extern ec_DataVtable vtable_eulerRotation; // vec3, additive
 extern ec_DataVtable vtable_scale; // vec3, multiplicative
 
+typedef void* (*fp_identity)(void* val_out);
+typedef void* (*fp_invert  )(void* val_inout);
+typedef void* (*fp_concat  )(void* val_out, const void* lhs, const void* rhs);
+typedef void* (*fp_scale   )(void* val_inout, const a3real scale);
+
 struct ec_DataVtable
 {
 	size_t size;
 
 	//1st order essential functions
-	void* (*identity)(void* val_out);
-	void* (*copy)(void* dst, const void* src);
-	void* (*invert)(void* val_inout);
-	void* (*concat)(void* val_out, const void* lhs, const void* rhs);
-	void* (*scale)(void* val_inout, const a3real scale);
+	fp_identity identity;
+	fp_invert   invert;
+	fp_concat   concat;
+	fp_scale    scale;
 
 	//Optional to override, vtable_setDefaults sets these to the functions below
 	
+	void* (*copy      )(void* dst, const void* src, const ec_DataVtable* funcs);
 	void* (*lerp      )(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs);
 	void* (*nearest   )(void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs);
 	void* (*cubic     )(void* val_out, const void* vals, const a3real param, const ec_DataVtable* funcs); //Assume vals is void[4]
@@ -47,6 +52,7 @@ struct ec_DataVtable
 };
 
 //Defaults
+void* defaultCopy      (void* dst, const void* src, const ec_DataVtable* funcs);
 void* defaultLerp      (void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs);
 void* defaultNearest   (void* val_out, const void* v0, const void* v1, const a3real param, const ec_DataVtable* funcs);
 void* defaultCubic     (void* val_out, const void* vals, const a3real param, const ec_DataVtable* funcs); //Assume vals is void[4]
